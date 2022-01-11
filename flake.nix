dd70718cac370d2bc44c1b45848969386282970b
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-01-11 20:13:19 +0100

    Make this repository a flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A flake for my version of tetris";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let pkgs = import nixpkgs { system = "x86_64-linux"; };
+    in
+    {
+      packages.x86_64-linux.tetris = pkgs.mkYarnPackage rec {
+        pname = "tetris";
+        version = "unstable";
+        src = ./.;
+        packageJSON = ./package.json;
+        yarnLock = ./yarn.lock;
+        yarnNix = ./yarn.nix;
+        buildPhase = "yarn run build";
+        installPhase = ''
+          cp -r deps/tetris/dist $out
+          rm $out/*.map
+        '';
+        distPhase = "true";
+      };
+      defaultPackage.x86_64-linux = self.packages.x64_64-linux.tetris;
+    };
+}

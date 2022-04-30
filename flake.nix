a0d6355481aa8b6058c501d0604551e45a329925
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-04-30 14:15:01 +0200

    Clean up flake.nix a little

--- flake.nix
+++ flake.nix
@@ -7,22 +7,24 @@
     let pkgs = import nixpkgs { system = "x86_64-linux"; };
     in
     {
-      packages.x86_64-linux.tetris = pkgs.mkYarnPackage rec {
-        pname = "tetris";
-        version = "unstable";
-        src = pkgs.lib.cleanSourceWith { filter = name: type: !(builtins.elem name [ ".github" "flake.lock" "flake.nix" ]); src = ./.; name = "source"; };
-        packageJSON = ./package.json;
-        yarnLock = ./yarn.lock;
-        yarnNix = ./yarn.nix;
-        buildPhase = "yarn run build";
-        installPhase = ''
-          cp -r deps/tetris/dist $out
-          rm $out/*.map
-        '';
-        distPhase = "true";
+      packages.x86_64-linux = rec {
+        default = tetris;
+        tetris = pkgs.mkYarnPackage rec {
+          pname = "tetris";
+          version = "unstable";
+          src = pkgs.lib.cleanSourceWith { filter = name: type: !(builtins.elem name [ ".github" "flake.lock" "flake.nix" ]); src = ./.; name = "source"; };
+          packageJSON = ./package.json;
+          yarnLock = ./yarn.lock;
+          yarnNix = ./yarn.nix;
+          buildPhase = "yarn run build";
+          installPhase = ''
+            cp -r deps/tetris/dist $out
+            rm $out/*.map
+          '';
+          distPhase = "true";
+        };
       };
-      defaultPackage.x86_64-linux = self.packages.x86_64-linux.tetris;
-      devShell.x86_64-linux = pkgs.mkShell {
+      devShells.x86_64-linux.default = pkgs.mkShell {
         buildInputs = [ pkgs.nixpkgs-fmt pkgs.yarn2nix pkgs.yarn ];
       };
     };

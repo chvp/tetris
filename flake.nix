b4f57cf95093449e0dfbcf2c106e305221e84b06
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-05-22 00:21:02 +0200

    Use devshell instead of pkgs.mkShell

--- flake.nix
+++ flake.nix
@@ -2,30 +2,49 @@
   description = "A flake for my version of tetris";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-  };
-  outputs = { self, nixpkgs }:
-    let pkgs = import nixpkgs { system = "x86_64-linux"; };
-    in
-    {
-      packages.x86_64-linux = rec {
-        default = tetris;
-        tetris = pkgs.mkYarnPackage rec {
-          pname = "tetris";
-          version = "unstable";
-          src = pkgs.lib.cleanSourceWith { filter = name: type: !(builtins.elem name [ ".github" "flake.lock" "flake.nix" ]); src = ./.; name = "source"; };
-          packageJSON = ./package.json;
-          yarnLock = ./yarn.lock;
-          yarnNix = ./yarn.nix;
-          buildPhase = "yarn run build";
-          installPhase = ''
-            cp -r deps/tetris/dist $out
-            rm $out/*.map
-          '';
-          distPhase = "true";
-        };
-      };
-      devShells.x86_64-linux.default = pkgs.mkShell {
-        buildInputs = [ pkgs.nixpkgs-fmt pkgs.yarn2nix pkgs.yarn ];
+    flake-utils.url = "github:numtide/flake-utils";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs = {
+        flake-utils.follows = "flake-utils";
+        nixpkgs.follows = "nixpkgs";
       };
     };
+  };
+  outputs = { self, nixpkgs, devshell, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; overlays = [ devshell.overlay ]; };
+      in
+      {
+        packages = rec {
+          default = tetris;
+          tetris = pkgs.mkYarnPackage rec {
+            pname = "tetris";
+            version = "unstable";
+            src = pkgs.lib.cleanSourceWith { filter = name: type: !(builtins.elem name [ ".github" "flake.lock" "flake.nix" ]); src = ./.; name = "source"; };
+            packageJSON = ./package.json;
+            yarnLock = ./yarn.lock;
+            yarnNix = ./yarn.nix;
+            buildPhase = "yarn run build";
+            installPhase = ''
+              cp -r deps/tetris/dist $out
+              rm $out/*.map
+            '';
+            distPhase = "true";
+          };
+        };
+        devShells = rec {
+          default = tetris;
+          tetris = pkgs.devshell.mkShell {
+            name = "Tetris";
+            packages = with pkgs; [
+              nixpkgs-fmt
+              yarn2nix
+              yarn
+            ];
+          };
+        };
+      }
+    );
 }
